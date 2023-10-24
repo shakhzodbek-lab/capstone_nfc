be8ae09d78309d3c908623a6d63b0b46f187bb80
Rakhmatov Shakhzodbek <63743504+shakhzodbek-lab@users.noreply.github.com>
2023-10-24 14:03:01 +0900

    Create sample.py

---
+++ sample.py
@@ -0,0 +1,42 @@
+import cv2
+import nfc
+
+# Initialize the NFC reader
+clf = nfc.ContactlessFrontend('usb')
+
+# Initialize the face detection model (you might need to adjust the path)
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+# Function to capture and detect faces
+def detect_face():
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        ret, frame = cap.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+
+        cv2.imshow('Face Detection', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+# NFC event handler
+def on_connect(tag):
+    print("NFC Card Detected:", tag)
+    detect_face()
+    # Send data to the server here (you'll need to implement the server connection)
+
+# Start the NFC event loop
+try:
+    clf.connect(rdwr={'on-connect': on_connect})
+except KeyboardInterrupt:
+    pass
+finally:
+    clf.close()
